52a83599f046902a443ec42bf7b9f87786b0c25f
jmishra <jmishra@lumenore.com>
2023-12-31 10:28:28 +0530

    add refcell struct example

---
+++ cell_struct/refcell_struct_01/src/main.rs
@@ -0,0 +1,43 @@
+use std::cell::RefCell;
+
+struct Software {
+    name: String,
+    version: usize,
+}
+
+impl Software {
+    fn show(&self) {
+        println!("{}: {}", self.name, self.version);
+    }
+}
+
+fn main() {
+    let software = Software {
+        name: "Apple".to_string(),
+        version: 1,
+    };
+    software.show();
+    let refcell = RefCell::new(software);
+
+    {
+        // Without curley bracket this code will not compile
+        let b = refcell.borrow();
+        b.show();
+    }
+    // OR, write in the below way
+    refcell.borrow().show();
+
+    update_name(&refcell);
+    
+    // Show updated value
+    refcell.borrow().show();
+}
+
+fn update_name(software: &RefCell<Software>) {
+    software.borrow_mut().name = "Microsoft".to_string();
+    update_version(software);
+}
+
+fn update_version(software: &RefCell<Software>) {
+    software.borrow_mut().version = 2;
+}
